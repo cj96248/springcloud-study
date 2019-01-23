a1228ffd4ef5a9f608d09ab7216e5d0d0ce39bd1
cj96248 <cj96248@sina.com>
2019-01-23 21:47:19 +0800

    添加Eureka配置

--- microservice-consumer/src/main/java/com/chao/consumer/config/ConfigBean.java
+++ microservice-consumer/src/main/java/com/chao/consumer/config/ConfigBean.java
@@ -1,10 +1,12 @@
 package com.chao.consumer.config;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableEurekaClient
 public class ConfigBean {
 
     @Bean
